f35871dfad0cfd8a8c3316dc791ea09cbe5bee32
oluwaseyipd <oluwaseyipd@gmail.com>
2026-06-02 06:48:48 +0100

    fix: update Python version to 3.12 and modify health check path in render.yaml; remove migration step from build script

--- build.sh
+++ build.sh
@@ -11,8 +11,4 @@ python -m pip install -r requirements.txt
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
-# Run migrations
-echo "Running migrations..."
-python manage.py migrate
-
 echo "Build complete!"
